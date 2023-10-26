50eb76f1de1391fa8da51c28a11b0c8fdf820fda
larry <26318510+larry0x@users.noreply.github.com>
2023-10-26 04:58:47 +0100

    delete verify methods (to use ics23 instead)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 mod set;
 mod tree;
 mod types;
-mod verify;
 
 pub use crate::{
     set::Set,
     tree::{Tree, TreeError},
     types::*,
-    verify::{verify_membership, verify_non_membership, VerificationError},
 };
